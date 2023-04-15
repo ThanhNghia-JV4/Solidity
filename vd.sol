dae26aa0f2a1a558552ba8e18bd557fe3dfb68e9
GroupWeb21 <80159815+ThanhNghia-JV4@users.noreply.github.com>
2023-04-15 22:29:38 +0700

    update

---
+++ vd.sol
@@ -0,0 +1,25 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Event{
+    // khai bao su kiến
+    event IntegersAdded(uint x, uint y, uint result);
+
+    function add(uint _x, uint _y) public returns (uint) {
+    uint result = _x + _y;
+    // kich hoat su kien cho ung dung dapp biet ham nay dang duoc chay
+    emit IntegersAdded(_x, _y, result);
+    return result;
+    }
+//code JS bat su kien
+//     YourContract.IntegersAdded(function(error, result) {
+//   // do something with result
+//     })
+
+    // Vi du: Trong mot app tai chinh. Ta lưu tru so du cua mot diachi dang uint
+    mapping (address => uint) public accountBalance;
+    // Hoac anh xa usernames voi userId
+    mapping (uint => string) userIdToName;                                                  
+
+}
+
+//chi la vi du 
